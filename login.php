5120ba55a9fc3c0bc6c46af79356532a90d08eef
toppy368 <toppy36849@gmail.com>
2015-05-15 23:49:04 +0800

    login.php：新增輸入框
    新增表單功能的輸入框及按鈕及相關設定，並新增Subit按鈕為登入鍵

--- login.php
+++ login.php
@@ -12,10 +12,10 @@
 		<!--HTML表單，使用者請在此欄輸入帳號密碼，初次登入請按上面的註冊紐-->
 		<h3>請輸入您的帳號密碼</h3></br>
 		<!--本表單將以POST方式傳送資料回本檔案-->
-		<form action="login.php" method="post">
-			帳號：</br>
-			密碼：</br>
-		
+			<form action="login.php" method="post">
+				帳號：<input type="text" name="account" /></br>
+				密碼：<input type="password" name="password" /></br>
+			<input type="submit" value="登入"></p>
 		
 	</body>
 </html>
