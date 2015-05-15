f8c9e129c7baf5928ed56d2b02994a87fe6b563f
toppy368 <toppy36849@gmail.com>
2015-05-15 22:37:38 +0800

    修正註解並寫入title標籤
    修正註解，META標籤在前、title在後，title會提醒訪客，此網頁的功能是登入帳號

--- login.php
+++ login.php
@@ -1,7 +1,9 @@
 <html>
 <head>
-	<!--This file is login.php-->
+	<!-- title And meat data -->
 	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!--This file is login.php-->
+		<title>登入帳號 - 帳號管理介面</title>
 </head>
 <body>
 
