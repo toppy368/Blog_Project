22d3358f7b2ed52e9259d87b7731c21c955ef851
toppy368 <toppy36849@gmail.com>
2015-05-16 00:00:05 +0800

    register.php：設定head資訊
    1. 寫入MEAT標籤並將編碼設定為UTF-8
    2. 設定title標籤

--- register.php
+++ register.php
@@ -1,8 +1,11 @@
 <html>
 <head>
-
+	<!-- title And meat data -->
+	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<!--This file is register.php-->
+		<title>註冊帳號 - 帳號管理介面</title>
 </head>
 	<body>
-
+		
 	</body>
 </html>
