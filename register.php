992aaad8ddb78906b5473f38497c2123deb9248b
toppy368 <toppy36849@gmail.com>
2015-05-25 23:46:49 +0800

    register.php：解決 Notice： Undefined index ... 的問題
    透過if來判斷，isset來偵測，如果表單按下submit按鈕，就會啟動此判斷式

--- register.php
+++ register.php
@@ -24,18 +24,19 @@
 				//測試PHP是否有作用
 				echo "hello world"."<p />";
 				
-				//解決 Notice： Undefined index ... 的問題
-				if (isset($_POST["submit"])) {
-				
+				//解決undefined index的問題
+				//用IF及isset來判斷，按下submit後馬上執行以下語法
+				if(isset($_POST['submit'] {
+					
+				}
+
 				//POST傳回本頁的標籤會變成 $_POST['變數名稱'];
 				//將POST的變數統一更改為 $post_變數名稱
 				$account = $_POST['account'];
 				$password = $_POST['password'];
 				$check_password = $_POST['check_password'];
 				
-				echo $account ;
-				}
-				
+
 				
 				//將SQL連線資訊重新定義
 				//本檔案以PDO法連線到SQL資料庫，此功能PHP5以上才支援
