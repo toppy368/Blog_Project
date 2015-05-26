e230b082133e55372216a972d7d4c61a96129e58
toppy368 <toppy36849@gmail.com>
2015-05-26 23:25:50 +0800

    index.phpl

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<!-- Title and meat data  -->
+<!-- This file is index.php -->
+	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>部落格首頁</title>
+</head>
+<body>
+<!--本專案使用GNU GENERAL PUBLIC LICENSE Version 2為授權條款，詳情請訪問此目錄的LICENSE.txt-->
+	<h1>Blog首頁</h1><p />
+
+</body>
+</html>
