13c8047cd7695a6c5f0dc0f194573a803242ef6b
toppy368 <toppy36849@gmail.com>
2015-05-29 02:10:40 +0800

    register.php：增加資料庫連線資訊
    增加資料庫連線資訊

--- register.php
+++ register.php
@@ -21,7 +21,20 @@
 		
 		<!--以下為PHP章節，將使用者填好的帳號密碼透過PHP寫入到MySQL資料庫-->
 		<?php
-
+			//PHP區域，本區域將連結phpMyAdmin，MySQL資料庫伺服器
+				
+				//phpMyAdmin主機連線內容資訊：
+				//
+				//本檔案以PDO法連線到SQL資料庫，此功能PHP5以上才支援
+				//如果你自行安裝Web伺服器後台，改了root帳號密碼，也請修改本檔案以下資訊
+				//左邊的參數會儲存右邊雙引號的連線資訊，您可以自行修改右邊雙引號的內容而不必修改整個檔案
+				//
+				/*資料庫連線位置：*/
+				$db_url = "mysql:host=localhost;dbname=blog_project";
+				/*資料庫管理員帳號：*/
+				$db_user = "root";											
+				/*資料庫管理員密碼：*/
+				$db_pw = "password";
 				
 
 		 ?>
