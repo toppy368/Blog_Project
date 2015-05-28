2b60ddd6c6a8dc7deb2be8e963b12330ab2723f5
toppy368 <toppy36849@gmail.com>
2015-05-29 02:05:56 +0800

    admin/post_new.php：關閉資料庫連結
    1. 在89行，將 $db_link設定為NULL，關閉資料庫連結
    
    新增文章的功能已經完成，本檔案將為資料庫寫入的範例，暫停更新，直到登入功能完成，才會繼續更新本檔案

--- admin/post_new.php
+++ admin/post_new.php
@@ -79,6 +79,15 @@
 				//以 execute() 方法執行 SQL 參數
 				$sql->execute();
 				
+				//關閉資料庫連結
+				//
+				//舊版mysql_close參數已於PHP5以上停用
+				//根據 php.net說明，PDO只要將連線指標設定為NULL即可關閉資料庫連結
+				//
+				//相關討論：
+				//http://stackoverflow.com/questions/1046614/do-sql-connections-opened-with-pdo-in-php-have-to-be-closed
+				$db_link = NULL;
+				
 				//以下PHP區域將做為驗證用途
 				//
 				//驗證範例網址：
