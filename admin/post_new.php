b56d6ee11ffb83b3adb0e4a79923873fbcffeb64
toppy368 <toppy36849@gmail.com>
2015-05-28 21:11:41 +0800

    admin\post_new.php：新增連線資訊
    在21行底下增加註解，說明此區將設定phpMyAdmin帳號連線各種資訊，以下資訊皆為示意圖，請根據您開發環境的相關設定自行修改以下語法

--- admin/post_new.php
+++ admin/post_new.php
@@ -18,7 +18,19 @@
 	
 	<?php
 	//PHP區域，本區域將連結phpMyAdmin，MySQL資料庫伺服器
-	
+				
+				//phpMyAdmin主機連線內容資訊：
+				//
+				//本檔案以PDO法連線到SQL資料庫，此功能PHP5以上才支援
+				//如果你自行安裝Web伺服器後台，改了root帳號密碼，也請修改本檔案以下資訊
+				//左邊的參數會儲存右邊雙引號的連線資訊，您可以自行修改右邊雙引號的內容而不必修改整個檔案
+				//
+				/*資料庫連線位置：*/
+				$db_url = "mysql:host=localhost;dbname=blog_project";
+				/*資料庫管理員帳號：*/
+				$db_user = "root";											
+				/*資料庫管理員密碼：*/
+				$db_pw = "password";	
 	?>
 </body>
 </html>
