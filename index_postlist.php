ba67c362b576952cc604a4cf735e08d41d6d5596
toppy368 <toppy36849@gmail.com>
2015-05-30 01:52:20 +0800

    index_postlist.php
    產生HTML標籤架構：
    1. 第9行註解為授權條款
    2. 第10行為功能說明及注意事項

---
+++ index_postlist.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<!-- Title and meat data  -->
+<!-- This file is index_postlist.php -->
+	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<?php
+//本專案使用GNU GENERAL PUBLIC LICENSE Version 2為授權條款，詳情請訪問此目錄的LICENSE.txt
+//本檔案為 index_* 群組下 ，將被index.php引用顯示本Blog所有文章，請勿放置到 admin 資料夾中 ! 
+
+?>
+</body>
+</html>
