617b085b1091428fe985cb04fe5af385c123b1c0
toppy368 <toppy36849@gmail.com>
2015-05-30 21:10:45 +0800

    index_postlist.php：增加註解
    在第12~20行增加註解，準便連接資料庫

--- index_postlist.php
+++ index_postlist.php
@@ -9,6 +9,16 @@
 //本專案使用GNU GENERAL PUBLIC LICENSE Version 2為授權條款，詳情請訪問此目錄的LICENSE.txt
 //本檔案為 index_* 群組下 ，將被index.php引用顯示本Blog所有文章，請勿放置到 admin 資料夾中 ! 
 	echo "<h1>"."Hello World"."</h1>"."<p />";
+	
+//PHP區域，本區域將連結phpMyAdmin，MySQL資料庫伺服器
+			
+		//phpMyAdmin主機連線內容資訊：
+		//
+		//本檔案以PDO法連線到SQL資料庫，此功能PHP5以上才支援
+		//如果你自行安裝Web伺服器後台，改了root帳號密碼，也請修改本檔案以下資訊
+		//左邊的參數會儲存右邊雙引號的連線資訊，您可以自行修改右邊雙引號的內容而不必修改整個檔案
+		//
+		
 ?>
 </body>
 </html>
