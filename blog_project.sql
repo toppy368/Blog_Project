8a845514e7715a21e2158e489a146f8726252bdf
toppy368 <toppy36849@gmail.com>
2015-05-30 21:45:07 +0800

    blog_project.sql
    修改資料表的部分欄位

--- blog_project.sql
+++ blog_project.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: 127.0.0.1
--- 產生時間： 2015 �?05 ??29 ??22:04
+-- 產生時間： 2015 �?05 ??30 ??21:43
 -- 伺服器版本: 5.6.24
 -- PHP 版本： 5.6.8
 -- 授權：GNU GENERAL PUBLIC LICENSE Version 2
@@ -30,9 +30,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `postdata` (
   `pid` int(11) NOT NULL,
+  `author_ID` char(15) COLLATE utf8_unicode_ci DEFAULT NULL,
   `title` char(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `context` text COLLATE utf8_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `context` text COLLATE utf8_unicode_ci,
+  `pdata` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -41,11 +43,48 @@ CREATE TABLE IF NOT EXISTS `postdata` (
 --
 
 CREATE TABLE IF NOT EXISTS `userdata` (
-  `aid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
   `account` char(15) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varbinary(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `password` char(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- 資料表的匯出資料 `userdata`
+--
+
+INSERT INTO `userdata` (`uid`, `account`, `password`) VALUES
+(1, '王小明', 'apple');
+
+--
+-- 已匯出資料表的索引
+--
+
+--
+-- 資料表索引 `postdata`
+--
+ALTER TABLE `postdata`
+  ADD PRIMARY KEY (`pid`), ADD KEY `author_ID` (`author_ID`);
+
+--
+-- 資料表索引 `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- 在匯出的資料表使用 AUTO_INCREMENT
+--
+
+--
+-- 使用資料表 AUTO_INCREMENT `postdata`
+--
+ALTER TABLE `postdata`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 使用資料表 AUTO_INCREMENT `userdata`
+--
+ALTER TABLE `userdata`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
