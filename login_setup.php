c4a0081247054d0be3c23d865e4ad8f9994922f4
toppy368 <toppy36849@gmail.com>
2015-06-14 21:57:01 +0800

    login功能修改
    1. 將login.php，更名為login_setup.php，並剪下HTML表格備用
    2. 建立login_form.html，並將剛才從login_setup.php剪下的HTML表格貼到這檔案上
    3.
    將register_form.html第12行"登入"的超連結設定為login_form.html，避免訪客點選超連結卻找不到檔案，造成404錯誤

--- login_setup.php
+++ login_setup.php
@@ -8,15 +8,7 @@
 	<body>
 <!--本專案使用GNU GENERAL PUBLIC LICENSE Version 2為授權條款，詳情請訪問此目錄的LICENSE.txt-->	
 		<h1>登入帳號 - 帳號管理介面</h1></p>
-		<!--此行可讓使用者切換登入及註冊功能，首次訪問的朋友請註冊帳號-->
-		<b>登入</b>　<a href="register_form.html">註冊</a></p>
-		<!--HTML表單，使用者請在此欄輸入帳號密碼-->
-		<h2>請輸入您的帳號密碼</h2></br>
-		<!--本表單將以POST方式傳送資料回本檔案-->
-			<form action="login.php" method="post">
-				帳號：<input type="text" name="account" /></br>
-				密碼：<input type="password" name="password" /></p>
-			<input type="submit" value="登入"></p>
+
 		<!--以下為PHP章節，此區的PHP負責核對使用者輸入的資料是否與資料庫的帳密相符合-->
 		<?php
 		//PHP區域，本區域將連結phpMyAdmin，MySQL資料庫伺服器
