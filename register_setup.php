0052216204d5d497f4eb64a5c5683b7000fed92b
toppy368 <toppy36849@gmail.com>
2015-06-14 21:49:49 +0800

    register.setup.php & register_form.html ：功能變更
    1. 將register_setup.php的HTML表格部分(11~20行)剪下
    2. 建立新檔案 register_form.html，並貼上剛才從register_setup.php剪下的HTML表格(11~20行)
    3. 將login.php的第20行"註冊"超連結，設定成register_form.html，避免無效連結造成404錯誤

--- register_setup.php
+++ register_setup.php
@@ -8,16 +8,6 @@
 	<body>
 <!--本專案使用GNU GENERAL PUBLIC LICENSE Version 2為授權條款，詳情請訪問此目錄的LICENSE.txt-->
 		<h1>註冊帳號 - 帳號管理介面</h1></p>
-		<!--功能切換區，已申請帳號的訪客，按下"登入"連結可以回到登入頁面-->
-		<a href="login.php">登入</a> <b>註冊</b></p>
-		<!--HTML表單，註冊用途-->
-		<h2>請輸入您想申請的帳號密碼</h2></p>
-		<!--本HTML表單將以POST方式回傳資料回本檔案-->
-		<form action="register_setup.php" method="post">
-		請輸入帳號：<input type="text" name="account" /><br />
-		請輸入密碼：<input type="password" name="password" /><br />
-		再輸入密碼：<input type="password" name="check_password" /><p />
-		<input type="submit" value="申請帳號"></p>
 		
 		<!--以下為PHP章節，將使用者填好的帳號密碼透過PHP寫入到MySQL資料庫-->
 		<?php			
